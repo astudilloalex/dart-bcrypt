82afa7307830093ac60bfd9046116dfa8fd94d25
Mateus Felipe <mateusfccp@gmail.com>
2025-02-06 06:58:32 -0300

    Validate input length on `hashpw` to avoid security issues

--- test/bcrypt_test.dart
+++ test/bcrypt_test.dart
@@ -13,5 +13,19 @@ void main() {
       expect(BCrypt.gensalt(prefix: '\$2x\$').startsWith('\$2x\$'), isTrue);
       expect(BCrypt.gensalt(prefix: '\$2y\$').startsWith('\$2y\$'), isTrue);
     });
+
+    test("`hashpw` shouldn't accept passwords with a length greater to 72 characters.", () {
+      final salt = BCrypt.gensalt();
+
+      expect(
+        () => BCrypt.hashpw('a' * 72, salt),
+        returnsNormally,
+      );
+
+      expect(
+        () => BCrypt.hashpw('a' * 73, salt),
+        throwsA(isA<ArgumentError>()),
+      );
+    });
   });
 }
